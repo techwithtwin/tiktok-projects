8180bd03772144dea802a36422f4c2d5eaf3f4d9
techwithtwin <techwithtwin@gmail.com>
2025-09-21 19:45:46 +0300

    add javascript age calculator

---
+++ js-age-calculator/script.js
@@ -0,0 +1,34 @@
+const form = document.querySelector("form");
+const result = document.querySelector(".result");
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(e) {
+  e.preventDefault();
+
+  const data = new FormData(e.target);
+  const birthDateStr = data.get("birth-date");
+
+  if (!birthDate){
+    alert('Invalid Data');
+    return;
+  }
+  const birthDate = new Date(birthDateStr);
+  const today = new Date();
+
+  // year difference
+  let age = today.getFullYear() - birthDate.getFullYear();
+
+  // lets check if the birthday has occurred.
+  const isMonthAhead = today.getMonth() > birthDate.getMonth();
+  const isCurrentMonth = today.getMonth() === birthDate.getMonth();
+  const isTodayAhead = today.getDate() > birthDate.getDate();
+
+  const hasBirthdayOccurred = isMonthAhead || isCurrentMonth || isTodayAhead;
+
+  if (!hasBirthdayOccurred) {
+    age--;
+  }
+
+  result.textContent = `You are ${age} year(s) old!`;
+}

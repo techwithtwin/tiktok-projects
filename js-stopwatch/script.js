ee70a902f75513980e9ded8e22472a595fb8c480
TechWithTwin <realwebtechguru@gmail.com>
2025-09-26 16:37:15 +0300

    init js

---
+++ js-stopwatch/script.js
@@ -0,0 +1,13 @@
+const startBtn = document.querySelector(".start");
+const stopBtn = document.querySelector(".stop");
+const resetBtn = document.querySelector(".reset");
+const timer = document.querySelector(".timer");
+
+startBtn.addEventListener("click", () => handleClick("start"));
+stopBtn.addEventListener("click", () => handleClick("stop"));
+resetBtn.addEventListener("click", () => handleClick("reset"));
+
+function handleClick(type) {
+  if (type === "start") {
+  }
+}

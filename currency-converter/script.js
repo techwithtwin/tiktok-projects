af0187881f7d0aee1d1d5e11a53996386684f01f
techwithtwin <techwithtwin@gmail.com>
2025-10-19 22:04:18 +0300

    adding currency converter

---
+++ currency-converter/script.js
@@ -0,0 +1,404 @@
+const currencies = [
+  {
+    symbol: "€",
+    name: "Euro",
+    symbol_native: "€",
+    decimal_digits: 2,
+    rounding: 0,
+    code: "EUR",
+    name_plural: "Euros",
+    type: "fiat",
+  },
+  {
+    symbol: "$",
+    name: "US Dollar",
+    symbol_native: "$",
+    decimal_digits: 2,
+    rounding: 0,
+    code: "USD",
+    name_plural: "US dollars",
+    type: "fiat",
+  },
+  {
+    symbol: "¥",
+    name: "Japanese Yen",
+    symbol_native: "￥",
+    decimal_digits: 0,
+    rounding: 0,
+    code: "JPY",
+    name_plural: "Japanese yen",
+    type: "fiat",
+  },
+  {
+    symbol: "BGN",
+    name: "Bulgarian Lev",
+    symbol_native: "лв.",
+    decimal_digits: 2,
+    rounding: 0,
+    code: "BGN",
+    name_plural: "Bulgarian leva",
+    type: "fiat",
+  },
+  {
+    symbol: "Kč",
+    name: "Czech Republic Koruna",
+    symbol_native: "Kč",
+    decimal_digits: 2,
+    rounding: 0,
+    code: "CZK",
+    name_plural: "Czech Republic korunas",
+    type: "fiat",
+  },
+  {
+    symbol: "Dkr",
+    name: "Danish Krone",
+    symbol_native: "kr",
+    decimal_digits: 2,
+    rounding: 0,
+    code: "DKK",
+    name_plural: "Danish kroner",
+    type: "fiat",
+  },
+  {
+    symbol: "£",
+    name: "British Pound Sterling",
+    symbol_native: "£",
+    decimal_digits: 2,
+    rounding: 0,
+    code: "GBP",
+    name_plural: "British pounds sterling",
+    type: "fiat",
+  },
+  {
+    symbol: "Ft",
+    name: "Hungarian Forint",
+    symbol_native: "Ft",
+    decimal_digits: 0,
+    rounding: 0,
+    code: "HUF",
+    name_plural: "Hungarian forints",
+    type: "fiat",
+  },
+  {
+    symbol: "zł",
+    name: "Polish Zloty",
+    symbol_native: "zł",
+    decimal_digits: 2,
+    rounding: 0,
+    code: "PLN",
+    name_plural: "Polish zlotys",
+    type: "fiat",
+  },
+  {
+    symbol: "RON",
+    name: "Romanian Leu",
+    symbol_native: "RON",
+    decimal_digits: 2,
+    rounding: 0,
+    code: "RON",
+    name_plural: "Romanian lei",
+    type: "fiat",
+  },
+  {
+    symbol: "Skr",
+    name: "Swedish Krona",
+    symbol_native: "kr",
+    decimal_digits: 2,
+    rounding: 0,
+    code: "SEK",
+    name_plural: "Swedish kronor",
+    type: "fiat",
+  },
+  {
+    symbol: "CHF",
+    name: "Swiss Franc",
+    symbol_native: "CHF",
+    decimal_digits: 2,
+    rounding: 0,
+    code: "CHF",
+    name_plural: "Swiss francs",
+    type: "fiat",
+  },
+  {
+    symbol: "Ikr",
+    name: "Icelandic Króna",
+    symbol_native: "kr",
+    decimal_digits: 0,
+    rounding: 0,
+    code: "ISK",
+    name_plural: "Icelandic krónur",
+    type: "fiat",
+  },
+  {
+    symbol: "Nkr",
+    name: "Norwegian Krone",
+    symbol_native: "kr",
+    decimal_digits: 2,
+    rounding: 0,
+    code: "NOK",
+    name_plural: "Norwegian kroner",
+    type: "fiat",
+  },
+  {
+    symbol: "kn",
+    name: "Croatian Kuna",
+    symbol_native: "kn",
+    decimal_digits: 2,
+    rounding: 0,
+    code: "HRK",
+    name_plural: "Croatian kunas",
+    type: "fiat",
+  },
+  {
+    symbol: "RUB",
+    name: "Russian Ruble",
+    symbol_native: "руб.",
+    decimal_digits: 2,
+    rounding: 0,
+    code: "RUB",
+    name_plural: "Russian rubles",
+    type: "fiat",
+  },
+  {
+    symbol: "TL",
+    name: "Turkish Lira",
+    symbol_native: "₺",
+    decimal_digits: 2,
+    rounding: 0,
+    code: "TRY",
+    name_plural: "Turkish Lira",
+    type: "fiat",
+  },
+  {
+    symbol: "AU$",
+    name: "Australian Dollar",
+    symbol_native: "$",
+    decimal_digits: 2,
+    rounding: 0,
+    code: "AUD",
+    name_plural: "Australian dollars",
+    type: "fiat",
+  },
+  {
+    symbol: "R$",
+    name: "Brazilian Real",
+    symbol_native: "R$",
+    decimal_digits: 2,
+    rounding: 0,
+    code: "BRL",
+    name_plural: "Brazilian reals",
+    type: "fiat",
+  },
+  {
+    symbol: "CA$",
+    name: "Canadian Dollar",
+    symbol_native: "$",
+    decimal_digits: 2,
+    rounding: 0,
+    code: "CAD",
+    name_plural: "Canadian dollars",
+    type: "fiat",
+  },
+  {
+    symbol: "CN¥",
+    name: "Chinese Yuan",
+    symbol_native: "CN¥",
+    decimal_digits: 2,
+    rounding: 0,
+    code: "CNY",
+    name_plural: "Chinese yuan",
+    type: "fiat",
+  },
+  {
+    symbol: "HK$",
+    name: "Hong Kong Dollar",
+    symbol_native: "$",
+    decimal_digits: 2,
+    rounding: 0,
+    code: "HKD",
+    name_plural: "Hong Kong dollars",
+    type: "fiat",
+  },
+  {
+    symbol: "Rp",
+    name: "Indonesian Rupiah",
+    symbol_native: "Rp",
+    decimal_digits: 0,
+    rounding: 0,
+    code: "IDR",
+    name_plural: "Indonesian rupiahs",
+    type: "fiat",
+  },
+  {
+    symbol: "₪",
+    name: "Israeli New Sheqel",
+    symbol_native: "₪",
+    decimal_digits: 2,
+    rounding: 0,
+    code: "ILS",
+    name_plural: "Israeli new sheqels",
+    type: "fiat",
+  },
+  {
+    symbol: "Rs",
+    name: "Indian Rupee",
+    symbol_native: "টকা",
+    decimal_digits: 2,
+    rounding: 0,
+    code: "INR",
+    name_plural: "Indian rupees",
+    type: "fiat",
+  },
+  {
+    symbol: "₩",
+    name: "South Korean Won",
+    symbol_native: "₩",
+    decimal_digits: 0,
+    rounding: 0,
+    code: "KRW",
+    name_plural: "South Korean won",
+    type: "fiat",
+  },
+  {
+    symbol: "MX$",
+    name: "Mexican Peso",
+    symbol_native: "$",
+    decimal_digits: 2,
+    rounding: 0,
+    code: "MXN",
+    name_plural: "Mexican pesos",
+    type: "fiat",
+  },
+  {
+    symbol: "RM",
+    name: "Malaysian Ringgit",
+    symbol_native: "RM",
+    decimal_digits: 2,
+    rounding: 0,
+    code: "MYR",
+    name_plural: "Malaysian ringgits",
+    type: "fiat",
+  },
+  {
+    symbol: "NZ$",
+    name: "New Zealand Dollar",
+    symbol_native: "$",
+    decimal_digits: 2,
+    rounding: 0,
+    code: "NZD",
+    name_plural: "New Zealand dollars",
+    type: "fiat",
+  },
+  {
+    symbol: "₱",
+    name: "Philippine Peso",
+    symbol_native: "₱",
+    decimal_digits: 2,
+    rounding: 0,
+    code: "PHP",
+    name_plural: "Philippine pesos",
+    type: "fiat",
+  },
+  {
+    symbol: "S$",
+    name: "Singapore Dollar",
+    symbol_native: "$",
+    decimal_digits: 2,
+    rounding: 0,
+    code: "SGD",
+    name_plural: "Singapore dollars",
+    type: "fiat",
+  },
+  {
+    symbol: "฿",
+    name: "Thai Baht",
+    symbol_native: "฿",
+    decimal_digits: 2,
+    rounding: 0,
+    code: "THB",
+    name_plural: "Thai baht",
+    type: "fiat",
+  },
+  {
+    symbol: "R",
+    name: "South African Rand",
+    symbol_native: "R",
+    decimal_digits: 2,
+    rounding: 0,
+    code: "ZAR",
+    name_plural: "South African rand",
+    type: "fiat",
+  },
+];
+const baseUrl = "https://api.freecurrencyapi.com/v1";
+
+const fromSelect = document.querySelector("#from");
+const toSelect = document.querySelector("#to");
+const form = document.querySelector("form");
+const result = document.querySelector(".result");
+const loader = document.querySelector(".loader");
+
+(function init() {
+  let options = "";
+  currencies.forEach((c) => {
+    options += `<option value="${c.code}">${c.code} — ${c.name}</option>`;
+  });
+
+  fromSelect.innerHTML = options;
+  toSelect.innerHTML = options;
+})();
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+
+  const from = formData.get("from");
+  const to = formData.get("to");
+  const amount = formData.get("amount");
+
+  if (from === to) {
+    alert("Error: Same currency convertion detected!");
+    return;
+  }
+
+  if (isNaN(Number(amount))) {
+    alert("Error: Invalid Number");
+    return;
+  }
+
+  loader.style.display = "block";
+  const data = await fetchConvertion(to, from);
+  loader.style.display = "none";
+
+  if (!data) {
+    alert("Failed to fetch");
+    return null;
+  }
+
+  console.log(data);
+
+  result.textContent = (data[to] * Number(amount)).toLocaleString();
+  result.style.display = "block";
+});
+
+async function fetchConvertion(to, from) {
+  try {
+    const res = await fetch(
+      `${baseUrl}/latest?currencies=${to}&base_currency=${from}`,
+      {
+        headers: {
+          apiKey: "fca_live_g65PNscr2UyLkVEB3QQHi7tRdhO6Xu4U27LdilbZ",
+        },
+      },
+    );
+
+    if (!res.ok)
+      throw new Error(`Failed to convert ${res.status} ${res.statusText}`);
+
+    const data = await res.json();
+
+    return data.data;
+  } catch (error) {
+    return null;
+  }
+}

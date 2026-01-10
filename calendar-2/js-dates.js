f0b381d9f94fd4578737711c087ce87d85137c22
techwithtwin <techwithtwin@gmail.com>
2026-01-10 16:23:04 +0300

    init calendar-2

---
+++ calendar-2/js-dates.js
@@ -0,0 +1,15 @@
+const date1 = new Date();
+
+console.log("Full Year", date1.getFullYear());
+
+console.log("Month", date1.getMonth());
+
+console.log("Week Day", date1.getDay());
+
+console.log("Hours", date1.getHours());
+console.log("Minutes", date1.getMinutes());
+
+console.log("Timestamp", date1.getTime());
+
+console.log("UTC date", date1.getUTCDate());
+console.log("Date", date1.getDate());
